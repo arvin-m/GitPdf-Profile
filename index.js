5a1bd86b7eea3a8f8a32730e126f4de88b826ef0
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-02-08 14:35:41 -0500

    adding some other code

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs=require("fs");
 const inquirer=require("inquirer");
 const axios =require("axios");
-// const html =require("./generateHTML.js");
+const generateHTML =require("./generateHTML.js");
+const util = require("util");
+const convertFactory = require('electron-html-to');
 
-
-
-
+const writeFileAsync = util.promisify(fs.writeFile);
 
 const questions = [
      {
@@ -18,7 +18,7 @@ const questions = [
     type: "list",
     name:"favColor",
     message:"What's You're Favorate Color ? " ,
-    choices: ["Red", "Pink", "Blue","Pink"],
+    choices: ["red", "pink", "blue","pink"],
     
   },
 
@@ -26,53 +26,61 @@ const questions = [
   
 ];
 
-
-function writeToFile(fileName, data) {
- 
+function promptUser(){
+   return inquirer.prompt(questions);
 }
 
-function init() {
+function apiCall(username){
+  const queryUrl = `https://api.github.com/users/${username}`;
+  return axios.get(queryUrl);
+}
 
-  
-inquirer
-.prompt(questions)
-.then(function({username,favColor}) {
-  console.log(username,favColor);
-    
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-    console.log(queryUrl);
 
-    // axios
-    //   .get(queryUrl)
-    //   .then(function(res) {
-    //     const repoNames = res.data.map(function(repo) {
-    //       return repo.name;
-    //     });
 
-    //     const repoNamesStr = repoNames.join("\n");
-
-    //     fs.writeFile("repos.txt", repoNamesStr, function(err) {
-    //       if (err) {
-    //         throw err;
-    //       }
-
-    //       console.log(`Saved ${repoNames.length} repos`);
-    //     }); 
-    //   });
+function writeToFile(filePath, htmlString) {
+  console.log(htmlString)
+  const conversion = convertFactory({
+    converterPath: convertFactory.converters.PDF
   });
 
+  conversion({ html: htmlString }, function(err, result) {
+    if (err) throw err;
 
-    
+    result.stream.pipe(fs.createWriteStream(filePath));
+    conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
+  });
 }
+
+ async function init() {
+   try 
+   {
+     const answers = await promptUser();
+     console.log(answers);
+     const apiResponse = await apiCall(answers.username);
+     console.log(apiResponse);
+     const data = apiResponse.data;
+     data.color = answers.favColor;
+
+     const htmlString = generateHTML(data);
+
+     //create an html file so we can check our work before we convert to a pdf
+     await writeFileAsync("./index.html", htmlString);
+     //errors will be caught below, otherwise, this console.log will run:
+     console.log("made a new file!");
+
+     writeToFile("./portfolio.pdf", htmlString);
+
+
+    }
+    catch (error) {
+      throw error;
+    }
+}
+
+
 
 init();
 
 
 
 
-
-
-
-
-// ==============================================================================================================================
-// ==============================================================================================================================
