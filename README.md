054b46bea879a9c3f894870cf0f02858c462e804
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-05-28 02:14:12 -0400

    adding to the readme file

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # NodeCLI-HTMLcreator
-Create an application that ask for  github user name and choosing a color to create an HTML web page and convert it to the PDF file.
+Create a command-line application that dynamically generates a PDF profile from a GitHub username.The user will be prompted for a favorite color, which will be used as the background color for cards.
+
+The PDF will be populated with the following:
+
+* Profile image
+* User name
+* Links to the following:
+  * User location via Google Maps
+  * User GitHub profile
+  * User blog
+* User bio
+* Number of public repositories
+* Number of followers
+* Number of GitHub stars
+* Number of users following
+
+
+## Installing
+- After you clone the repo you will run npm i in your terminal to downlaod NPM packeges
+- In your terminal you can run npm start to run the application
+
+## Built With 
+    JavaScript, HTML, Node.js, axios, electron-html-to, inquirer,GitHub user API
+
+## Example of Generating the PDF
+[GitHub](https://arvin-m.github.io/GitPdf-Profile)
+
+
--- index.js
+++ index.js
@@ -1,83 +1,72 @@
-const fs=require("fs");
-const inquirer=require("inquirer");
-const axios =require("axios");
-const generateHTML =require("./generateHTML.js");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const generateHTML = require("./generateHTML.js");
 const util = require("util");
 const convertFactory = require('electron-html-to');
-
 const writeFileAsync = util.promisify(fs.writeFile);
-
 const questions = [
-     {
+  {
     type: "input",
-    name:"username",
-    message:"What's You're GitHub User Name? " ,
-    
+    name: "username",
+    message: "What's You're GitHub User Name? ",
+
   },
   {
     type: "list",
-    name:"favColor",
-    message:"What's You're Favorate Color ? " ,
-    choices: ["red", "pink", "blue","pink"],
-    
+    name: "favColor",
+    message: "What's You're Favorate Color ? ",
+    choices: ["red", "pink", "blue", "pink"],
+
   },
 
-
-  
 ];
 
-function promptUser(){
-   return inquirer.prompt(questions);
-}
+function promptUser() {
+  return inquirer.prompt(questions);
+};
 
-function apiCall(username){
+function apiCall(username) {
   const queryUrl = `https://api.github.com/users/${username}`;
   return axios.get(queryUrl);
-}
-
-
+};
 
 function writeToFile(filePath, htmlString) {
-  // console.log(htmlString)
   const conversion = convertFactory({
     converterPath: convertFactory.converters.PDF
   });
 
-  conversion({ html: htmlString }, function(err, result) {
+  conversion({ html: htmlString }, function (err, result) {
     if (err) throw err;
 
     result.stream.pipe(fs.createWriteStream(filePath));
     conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
   });
-}
+};
 
- async function init() {
-   try 
-   {
-     const answers = await promptUser();
+async function init() {
+  try {
+    const answers = await promptUser();
     //  console.log(answers);
-     const apiResponse = await apiCall(answers.username);
+    const apiResponse = await apiCall(answers.username);
     //  console.log(apiResponse);
-     const data = apiResponse.data;
-     data.color = answers.favColor;
+    const data = apiResponse.data;
+    data.color = answers.favColor;
 
-     const htmlString = generateHTML(data);
+    const htmlString = generateHTML(data);
 
-     //create an html file so we can check our work before we convert to a pdf
-     await writeFileAsync("./index.html", htmlString);
-     //errors will be caught below, otherwise, this console.log will run:
-     console.log("made a new file!");
+    //create an html file so we can check our work before we convert to a pdf
+    await writeFileAsync("./index.html", htmlString);
+    //errors will be caught below, otherwise, this console.log will run:
+    console.log("made a new file!");
 
-     writeToFile("./portfolio.pdf", htmlString);
+    writeToFile("./portfolio.pdf", htmlString);
 
-
-    }
-    catch (error) {
-      throw error;
-    }
+  }
+  catch (error) {
+    throw error;
+  }
 }
-
-
 
 init();
 
